af9b08e5671ee18f9ca60d4d5d74f1a581154453
shangrut <shangrut@usc.edu>
2018-10-01 23:10:30 -0700

    init components for startup window

--- PHYS495_CircuitAnalyzer/src/circuit_gui/StartupWindow.java
+++ PHYS495_CircuitAnalyzer/src/circuit_gui/StartupWindow.java
@@ -12,6 +12,21 @@ public class StartupWindow extends JFrame{
 	private JSlider frequencySlider;
 	private JLabel voltageLabel;
 	private JLabel frequencyLabel;
+	private JLabel impedanceLabel;
+	private JLabel amplitudeLabel;
+	private JLabel angleLabel;
+	private JButton addSingleComponent;
+	private JButton deleteSingleComponent;
+	private JButton addParallelComponents;
+	private JButton deleteParallelComponents;
+	static final int MIN_FREQ = 0;
+	static final int MAX_FREQ = 100000;
+	static final int DEFAULT_FREQ = 1000;
+	
+	static final int MIN_VOLTAGE = 1;
+	static final int MAX_VOLTAGE = 100;
+	static final int DEFAULT_VOLTAGE = 5;
+	
 
 	public static void main(String[] args) {
 		System.out.println("start" + 0);
@@ -24,6 +39,43 @@ public class StartupWindow extends JFrame{
 		setLocation(100,100);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(false);
-		this.setVisible(true);
+		initComponents();
+		
+		//this.setVisible(true);
+	}
+	
+	private void initComponents() {
+		voltageSlider = new JSlider(JSlider.HORIZONTAL,MIN_VOLTAGE,MAX_VOLTAGE,DEFAULT_VOLTAGE);
+		frequencySlider = new JSlider(JSlider.HORIZONTAL,MIN_FREQ,MAX_FREQ,DEFAULT_FREQ);
+		
+		voltageSlider.setMinorTickSpacing(10);
+		frequencySlider.setMinorTickSpacing(1);
+		
+		voltageSlider.setMajorTickSpacing(10);
+		frequencySlider.setMajorTickSpacing(10000);
+		
+		voltageSlider.setPaintTicks(true);
+		frequencySlider.setPaintTicks(false);
+		
+		voltageSlider.setPaintLabels(true);
+		frequencySlider.setPaintLabels(false);
+		
+		addSingleComponent = new JButton("Add single component");
+		deleteSingleComponent = new JButton("Delete single component");
+		addParallelComponents = new JButton("Add a parallel section");
+		deleteParallelComponents = new JButton("Delete a parallel section");
+		
+		frequencyLabel = new JLabel("Frequency: 1000Hz",JLabel.CENTER);
+		voltageLabel = new JLabel("Peak to peak voltage: 5V",JLabel.CENTER);
+		
+		impedanceLabel = new JLabel("Impedance:",JLabel.CENTER);
+		amplitudeLabel = new JLabel("Current Amplitude:",JLabel.CENTER);
+		angleLabel = new JLabel("Phase Angle:",JLabel.CENTER);
+		
+		
+	}
+	
+	private void createGUI() {
+		
 	}
 }
