cccdee6b2f7bdb2e159a9cbdd1efc2aff6d84d2b
shangrut <shangrut@usc.edu>
2018-11-14 01:53:21 -0800

    changed layout of view circuit window

--- PHYS495_CircuitAnalyzer/src/circuit_gui/ViewCircuitWindow.java
+++ PHYS495_CircuitAnalyzer/src/circuit_gui/ViewCircuitWindow.java
@@ -10,7 +10,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 import circuit_logic.Circuit;
 import circuit_logic.CircuitComponent;
